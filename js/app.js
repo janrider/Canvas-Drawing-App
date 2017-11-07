44d9acb2a990516b07e17397b99c8e5d1eb42ff7
Vlad Levkutnyk <vladyslav.levkutnyk@auto1.com>
2017-11-07 17:17:12 +0100

    follow cursor drawing

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ canvas.width = wW;
 canvas.height = wH;
 
 canvas.isDraw = false;
-canvas.source = 0;
+canvas.source = '0';
 
 if (canvas.getContext) {
   var ctx = canvas.getContext("2d");
@@ -31,17 +31,41 @@ if (canvas.getContext) {
     canvas.opacity = slider.value;
   }
 
-  canvas.drawLine = function() {
+  canvas.drawLine = function(mX, mY) {
     ctx.beginPath();
-    ctx.moveTo(wW * koof(), wH * koof());
-    ctx.lineTo(wW * koof(), wH * koof());
+    var line = {
+      sX: wW * koof(),
+      sY: wH * koof(),
+      eX: wW * koof(),
+      eY: wH * koof(),
+    }
+
+    if (mX && canvas.source === '2') {
+      var S = function() {
+        return Math.floor((Math.random() * (40 - 5) + 5) * (Math.random() < 0.5 ? -1 : 1));
+      };
+      var kX = line.sX - line.eX;
+      var kY = line.sY - line.eY;
+      var newLine = {
+        sX: mX + S() - kX / 2,
+        sY: mY + S() - kY / 2,
+        eX: mX + S() + kX / 2,
+        eY: mY + S() + kY / 2,
+      };
+      ctx.moveTo(newLine.sX, newLine.sY);
+      ctx.lineTo(newLine.eX, newLine.eY);
+    } else {
+      ctx.moveTo(line.sX, line.sY);
+      ctx.lineTo(line.eX, line.eY);
+    }
+
     ctx.strokeStyle = 'rgba(' + color() + ', ' + color() + ', ' + color() + ', ' + (C(canvas.opacity)) + ')';
     ctx.stroke();
   }
 
-  canvas.moveOnCanvas = function() {
-    if (canvas.isDraw && canvas.source == 1) {
-      canvas.drawLine()
+  canvas.moveOnCanvas = function(e) {
+    if (canvas.isDraw && canvas.source > 0) {
+      canvas.drawLine(e.pageX, e.pageY)
     }
   }
 
@@ -57,7 +81,7 @@ if (canvas.getContext) {
     if (val) {
       nav.classList.add('active')
 
-      if (canvas.source === 0) {
+      if (canvas.source === '0') {
         canvas.interval = setInterval(function() {
           canvas.drawLine()
         }, SPEED)
@@ -73,4 +97,8 @@ if (canvas.getContext) {
   canvas.clearAll = function() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
+
+  canvas.addEventListener('mousemove', function(e){
+    canvas.moveOnCanvas(e);
+  })
 }
