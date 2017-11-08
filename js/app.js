e50ee966f193c95c824df694fcdd9bd97e10f2dc
Vlad Levkutnyk <vladyslav.levkutnyk@auto1.com>
2017-11-08 12:03:10 +0100

    mouseDown feature

--- js/app.js
+++ js/app.js
@@ -4,11 +4,15 @@ var nav = document.querySelector("nav");
 var wW = window.innerWidth;
 var wH = window.innerHeight;
 
+const SPEED = 1;
+
 canvas.width = wW;
 canvas.height = wH;
-
+canvas.opacity = 0.65;
+canvas.length = 200;
 canvas.isDraw = false;
-canvas.source = '0';
+canvas.source = '2';
+canvas.mousePress = false;
 
 if (canvas.getContext) {
   var ctx = canvas.getContext("2d");
@@ -18,73 +22,82 @@ if (canvas.getContext) {
   }
   var C = function(opacity) {
     return opacity + Math.round(Math.random()) * 0.4;
-  }
+  };
   var color = function() {
     return Math.round(Math.random() * (120 - 10) + 10);
-  }
-
-  const SPEED = 1;
-
-  canvas.opacity = 0.2;
-
-  canvas.setOpacity = function (slider) {
-    canvas.opacity = slider.value;
-  }
+  };
 
   canvas.drawLine = function(mX, mY) {
     ctx.beginPath();
-    var line = {
-      sX: wW * koof(),
-      sY: wH * koof(),
-      eX: wW * koof(),
-      eY: wH * koof(),
+
+    var float = function () {
+      return Math.floor((Math.random() * (15) + 5) * (Math.random() < 0.5 ? -1 : 1));
+    };
+
+    const point = {
+      x: (mX && mY) ? mX + float() : Math.floor(Math.random() * (wW) * 100) / 100,
+      y: (mX && mY) ? mY + float() : Math.floor(Math.random() * (wH) * 100) / 100,
+    };
+
+    const sign = function() {
+      return (Math.random() < 0.5 ? -1 : 1);
     }
 
-    if (mX && canvas.source === '2') {
-      var S = function() {
-        return Math.floor((Math.random() * (40 - 5) + 5) * (Math.random() < 0.5 ? -1 : 1));
-      };
-      var kX = line.sX - line.eX;
-      var kY = line.sY - line.eY;
-      var newLine = {
-        sX: mX + S() - kX / 2,
-        sY: mY + S() - kY / 2,
-        eX: mX + S() + kX / 2,
-        eY: mY + S() + kY / 2,
-      };
-      ctx.moveTo(newLine.sX, newLine.sY);
-      ctx.lineTo(newLine.eX, newLine.eY);
-    } else {
-      ctx.moveTo(line.sX, line.sY);
-      ctx.lineTo(line.eX, line.eY);
-    }
+    const signX = sign();
+    const signY = sign();
+
+    const lineSize = function() {
+      return Math.floor((Math.random() * canvas.length));
+    };
+
+    const line = {
+      sX: point.x + lineSize() * signX,
+      sY: point.y + lineSize() * signY,
+      eX: point.x + lineSize() * -signX,
+      eY: point.y + lineSize() * -signY,
+    };
+
+    ctx.moveTo(line.sX, line.sY);
+    ctx.lineTo(line.eX, line.eY);
 
     ctx.strokeStyle = 'rgba(' + color() + ', ' + color() + ', ' + color() + ', ' + (C(canvas.opacity)) + ')';
     ctx.stroke();
-  }
+  };
+
+  canvas.setOpacity = function (slider) {
+    canvas.opacity = slider.value;
+  };
+
+  canvas.setLength = function (slider) {
+    canvas.length = slider.value;
+  };
 
   canvas.moveOnCanvas = function(e) {
-    if (canvas.isDraw && canvas.source > 0) {
+    if (
+      canvas.isDraw &&
+      (canvas.source === '2' || (canvas.source === '1' && canvas.mousePress))
+    ) {
       canvas.drawLine(e.pageX, e.pageY)
     }
-  }
+  };
 
   canvas.setSource = function(selectObject) {
+    clearInterval(canvas.interval);
+
     canvas.source = selectObject.value;
-  }
+
+    canvas.setDraw(true);
+  };
 
   canvas.setDraw = function(val) {
     canvas.isDraw = val;
-    document.getElementById("startBtn").disabled = val;
-    document.getElementById("selectBtn").disabled = val;
-    document.getElementById("clearBtn").disabled = val;
     if (val) {
       nav.classList.add('active')
 
       if (canvas.source === '0') {
         canvas.interval = setInterval(function() {
           canvas.drawLine()
-        }, SPEED)
+        }, Math.floor(Math.random() * (SPEED - (SPEED / 2) + (SPEED / 2)) * 100) / 100)
       }
     } else {
       nav.classList.remove('active')
@@ -98,7 +111,18 @@ if (canvas.getContext) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
   }
 
-  canvas.addEventListener('mousemove', function(e){
-    canvas.moveOnCanvas(e);
-  })
+  canvas.mouseDown = function() {
+    canvas.mousePress = true;
+  };
+  canvas.mouseUp = function() {
+    canvas.mousePress = false;
+  };
+
+  canvas.addEventListener('mousedown', canvas.mouseDown);
+  canvas.addEventListener('mousemove', canvas.moveOnCanvas);
+  canvas.addEventListener('touchstart', canvas.mouseDown);
+  canvas.addEventListener('touchmove', canvas.moveOnCanvas);
+
+  window.addEventListener('mouseup', canvas.mouseUp);
+  window.addEventListener('touchend', canvas.mouseUp);
 }
